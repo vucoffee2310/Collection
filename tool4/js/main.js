201bb2589e1253634cd22fd66104d12e1a4dbfb5
vucoffee2310 <vucoffee2310@gmail.com>
2025-10-08 19:45:44 +0700

    Delete tool4 directory

--- tool4/js/main.js
+++
@@ -1,8 +0,0 @@
-// js/main.js
-import { PDFOverlayApp } from './app/PDFOverlayApp.js';
-import { jsonData } from './data.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    // Initialize the application with the original, unmodified data.
-    new PDFOverlayApp(jsonData);
-});
